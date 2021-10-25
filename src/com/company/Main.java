0fffab0dcfeaf0686bdfb07f8149c3b578ff78fe
Xan80 <sxsamedov@gmail.com>
2021-10-25 22:13:56 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+
+   public static void println1 (String line) {
+       System.out.println(line);
+   }
+
+
+    public static String println2 (String line) {
+        String text="AAAAAAAAAAAA";
+        System.out.println(text);
+
+        return text;
+
+   }
+
+    public static void println3 (int number) {
+
+        System.out.println("Task N"+ number);
+    }
+
+
+
+
+        public static void main(String[] args) {
+        println1("________________________________");
+       println2("");
+            println3(5);
+        System.out.println();
+
+
+   }
+}
